520125f55efca4cda11356e5730f13d00f9a64a5
Oliver <oliverloye@me.com>
2017-09-12 15:35:38 +0200

    Facade og interface implementering

---
+++ CA1/CA1/src/main/java/facade/IPersonFacade.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package facade;
+
+import entity.Company;
+import entity.Hobby;
+import entity.Person;
+import java.util.List;
+import javax.persistence.EntityManagerFactory;
+
+/**
+ *
+ * @author Oliver
+ */
+public interface IPersonFacade {
+    
+    public void addEntityManagerFactory(EntityManagerFactory emf);
+    public Person getPersonById(int id);
+    public Person getPersonByPhone(String phone);
+    public List<Person> getPersons();
+    public List<Person> getPersons(int zipCode);
+    public List<Person> getPersons(String hobbyName);
+    public int getNumPerson(String hobbyName);
+    
+
+}
