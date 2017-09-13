b199f0227e9dfb522da0c7417ab8cc373d23ef88
Xu Zhen Yang <xzy@Xus-MacBook-Pro.local>
2017-09-13 13:35:18 +0200

    updated

--- CA1/CA1/src/main/java/facade/FacadePerson.java
+++ CA1/CA1/src/main/java/facade/FacadePerson.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package facade;
 
 import entity.Person;
@@ -10,10 +5,6 @@ import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 
-/**
- *
- * @author Oliver
- */
 public class FacadePerson implements IPersonFacade {
     
     private EntityManagerFactory emf;
@@ -123,5 +114,39 @@ public class FacadePerson implements IPersonFacade {
         finally {
             em.close();
         }
+    }
+
+    @Override
+    public void addPerson(Person person) {
+        EntityManager em = emf.createEntityManager();
+
+        try {
+            em.getTransaction().begin();
+            em.persist(person);
+            em.getTransaction().commit();
+        }
+        finally {
+            em.close();
+        }
+    }
+
+    @Override
+    public Person editPerson(Person person) {
+        EntityManager em = emf.createEntityManager();
+
+        try {
+            em.getTransaction().begin();
+            Person p = em.find(Person.class, person.getId());
+            if(p != null) {
+                p = person;
+                em.merge(p);
+                em.getTransaction().commit();
+                return p;
+            }
+        }
+        finally {
+            em.close();
+        }  
+        return null;
     }
 }
